a34f78b8573f5d736722862026ca613ad15aad41
feddorovich <fedsygreen@mail.ru>
2023-01-16 13:46:18 +0300

    Gitignore

---
+++ String repeat.js
@@ -0,0 +1,13 @@
+function repeatStr (n, s) {
+    let str = "";
+    for (let i = 0; i < n; i++) {
+      str += s;
+    }
+    return str;
+  }
+
+  /*
+  function repeatStr (n, s) {
+  return s.repeat(n);
+}
+*/
