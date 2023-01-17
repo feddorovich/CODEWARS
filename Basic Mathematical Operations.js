461eee273c048e4e2df15e8702ce6d0e863622df
feddorovich <fedsygreen@mail.ru>
2023-01-17 16:07:12 +0300

    JS Tests

---
+++ Basic Mathematical Operations.js
@@ -0,0 +1,32 @@
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+      case "+":
+        return value1 + value2;
+      case "-":
+        return value1 - value2;
+      case "*":
+        return value1 * value2;
+      case "/":
+        return value1 / value2;
+      default:
+        return 0;
+    }
+  }
+
+/* 
+function basicOp(operation, value1, value2)
+{
+  if( operation == "+") {
+    return value1 + value2
+  }
+  if( operation == "-") {
+    return value1 - value2
+  }
+  if( operation == "*") {
+    return value1 * value2
+  }
+  if( operation == "/") {
+    return value1 / value2
+  }
+}
+*/
