f4d447497326bc57ac714cf4cbc91abae6e7f8c0
feddorovich <fedsygreen@mail.ru>
2023-01-22 18:23:58 +0300

    JS Tests

--- Abbreviate a Two Word Name.js
+++ Abbreviate a Two Word Name.js
@@ -1,28 +1,5 @@
 function abbrevName(name){
-    let [first,last] = name.split(' ')
-    return first[0].toUpperCase() + '.' + last[0].toUpperCase()
+    let [first, last] = name.split(" ");
+    return first[0].toUpperCase() + "." + last[0].toUpperCase();
+    
   }
-
-/*
-
-function abbrevName(name){
-
-  var nameArray = name.split(" ");
-  return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
-}
-
-or
-
-function abbrevName(name){
-
-    return name.split(' ').map(i => i[0].toUpperCase()).join('.')
-
-}
-
-or
-
-function abbrevName(name){
-  return name.split(' ').map(x => x.substr(0, 1).toUpperCase()).join('.');
-}
-
-*/
--- bbreviate a Two Word Name.js
+++
@@ -1,5 +0,0 @@
-function abbrevName(name){
-    let [first, last] = name.split(" ");
-    return first[0].toUpperCase() + "." + last[0].toUpperCase();
-    
-  }
