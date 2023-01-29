9c6c0011eea75f8fd22ef5d28c3a4af8449f7dfb
feddorovich <feddorovich@outlook.com>
2023-01-29 17:15:23 +0300

    JS Tests

--- JavaScript/7 kyu/List Filtering.js
+++ JavaScript/7 kyu/List Filtering.js
@@ -7,5 +7,11 @@ function filter_list(l) {
     }
     return result
 }
-
 console.log(filter_list([12, 'a', 15]))
+
+// or
+
+function filter_list2(l) {
+    return l.filter(function(v) {return typeof v == 'number'})
+}
+console.log(filter_list2([12, 'a', 15]))
