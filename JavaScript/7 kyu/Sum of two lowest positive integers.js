9fd544b0e8bd4eb256a23599bacea3cd70f3137c
feddorovich <feddorovich@outlook.com>
2023-02-12 21:56:47 +0300

    JS Tests

--- JavaScript/7 kyu/Sum of two lowest positive integers.js
+++ JavaScript/7 kyu/Sum of two lowest positive integers.js
@@ -4,3 +4,21 @@ function sumTwoSmallestNumbers(numbers) {
 }
 
 console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))
+
+/*
+function sumTwoSmallestNumbers(numbers) {
+    // numbers = numbers.sort((a, b) => a - b)
+    for (let j = 0; j < numbers.length; j++) {
+        for (let i = 0; i < numbers.length; i++) {
+            if(numbers[i] > numbers[i + 1]){
+                let temp = numbers[i]
+                numbers[i] = numbers[i + 1]
+                numbers[i + 1] = temp
+            }
+        }
+    }
+
+    return numbers[0] + numbers[1]
+
+    Сортировка пузырьком
+}*/
