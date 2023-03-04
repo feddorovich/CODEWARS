6930e7d3cd5764b3d81eb825922184fce82ad5ad
feddorovich <feddorovich@outlook.com>
2023-03-04 11:57:37 +0300

    JS Tests

---
+++ JavaScript/8 kyu/Rock Paper Scissors.js
@@ -0,0 +1,13 @@
+const rps = (p1, p2) => {
+    if (p1 === 'rock' && p2 === 'scissors' || p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock') {
+        return 'Player 1 won!'
+    }
+    if (p1 === 'rock' && p2 === 'paper' || p1 === 'paper' && p2 === 'scissors' || p1 === 'scissors' && p2 === 'rock') {
+        return 'Player 2 won!'
+    }
+    if (p1 === 'paper' && p2 === 'paper' || p1 === 'rock' && p2 === 'rock' || p1 === 'scissors' && p2 === 'scissors') {
+        return 'Draw!'
+    }
+};
+
+console.log(rps('rock', 'scissors'))
